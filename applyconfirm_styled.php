0a59d65c83eb79146439030cc49aff6e8213a9a7
SoujuRei <thelied1st@gmail.com>
2025-03-29 05:00:02 +0700

    Add files via upload

---
+++ applyconfirm_styled.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE HTML>
+<HTML lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="creating Web Applications">
+	<meta name="keywords" content="HTML, CSS, Javascript">
+	<meta name="author" content="Minh">
+	<link href="style/style.css" rel="stylesheet">
+	<link href="style/apply.css" rel="stylesheet">
+	<title>Application Confirmation</title>
+
+    <link href="style/confirm.css" rel="stylesheet"/>
+</head>
+<?php
+session_start();
+?>
+<body>
+<div  class = "cbox">
+<!--Encase in a box-->
+<section class="title">
+    <h2 id="text1">Application Confirmation</h2>
+    <a id="text2">
+    <?php
+    if (isset($_SESSION['success'])) {
+        echo "<h3 style='color: green;'>" . $_SESSION['success'] . "</h3>";
+        unset($_SESSION['success']); // Clear success message
+    } else {
+        echo "<p>No form submission detected.</p>";
+    }
+   
+
+    ?> </a>
+    <a href="apply.php">
+    <div class="returnbox">
+    Return
+    
+    </div>
+    </a>
+    </section>
+    
+</div>
+
+</body>
+</HTML>
