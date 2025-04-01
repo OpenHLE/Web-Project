4f0e7f8c13b345a40f923e90b193ba4404d8f193
OpenHLE <htlburner1@protonmail.com>
2025-04-01 15:18:12 +0700

    change back to mercury database

--- settings.php
+++ settings.php
@@ -1,15 +1,15 @@
 <?php
 // Database connection parameters (change if needed)
 
-//$host = "feenix-mariadb.swin.edu.au";
-//$user = "s105550047";      
-//$pwd = "team20assign2";     
-//$sql_db = "s105550047_db";
-
-$host = "localhost";
-$user = "root";     
-$pwd = "";          
+$host = "feenix-mariadb.swin.edu.au";
+$user = "s105550047";      
+$pwd = "team20assign2";     
 $sql_db = "s105550047_db";
+
+//$host = "localhost";
+//$user = "root";     
+//$pwd = "";          
+//$sql_db = "s105550047_db";
 ?> 
 
 
